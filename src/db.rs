463e6d9753898365392c29e9ee7a81397c58e27c
Richard Cook <rcook@rcook.org>
2021-03-17 13:43:20 -0700

    Create schema in db module

--- src/db.rs
+++ src/db.rs
@@ -26,6 +26,35 @@ pub struct ItemTag {
     pub tag_id: i32,
 }
 
+pub fn create_schema(conn: &Connection) -> Result<()> {
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS items (
+            id          INTEGER PRIMARY KEY,
+            location    TEXT NOT NULL UNIQUE,
+            signature   TEXT NOT NULL UNIQUE
+        )",
+        params![],
+    )?;
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS tags (
+            id          INTEGER PRIMARY KEY,
+            name        TEXT NOT NULL UNIQUE
+        )",
+        params![],
+    )?;
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS item_tags (
+            id          INTEGER PRIMARY KEY,
+            item_id     INTEGER NOT NULL,
+            tag_id      INTEGER NOT NULL,
+            FOREIGN KEY(item_id) REFERENCES items(id),
+            FOREIGN KEY(tag_id) REFERENCES tags(id)
+        )",
+        params![],
+    )?;
+    Ok(())
+}
+
 impl Item {
     #[allow(dead_code)]
     pub fn insert(conn: &Connection, item: &item::Item) -> Result<()> {
--- src/project.rs
+++ src/project.rs
@@ -1,6 +1,7 @@
-use rusqlite::{params, Connection};
+use rusqlite::Connection;
 use std::path::{Path, PathBuf};
 
+use crate::db::create_schema;
 use crate::error::Result;
 use crate::walk::{ExtensionSet, SampleVisitor};
 
@@ -20,31 +21,7 @@ impl Project {
 
     pub fn open_db_connection(&self) -> Result<Connection> {
         let conn = Connection::open(&self.db_path)?;
-        conn.execute(
-            "CREATE TABLE IF NOT EXISTS items (
-                id          INTEGER PRIMARY KEY,
-                location    TEXT NOT NULL UNIQUE,
-                signature   TEXT NOT NULL UNIQUE
-            )",
-            params![],
-        )?;
-        conn.execute(
-            "CREATE TABLE IF NOT EXISTS tags (
-                id          INTEGER PRIMARY KEY,
-                name        TEXT NOT NULL UNIQUE
-            )",
-            params![],
-        )?;
-        conn.execute(
-            "CREATE TABLE IF NOT EXISTS item_tags (
-                id          INTEGER PRIMARY KEY,
-                item_id     INTEGER NOT NULL,
-                tag_id      INTEGER NOT NULL,
-                FOREIGN KEY(item_id) REFERENCES items(id),
-                FOREIGN KEY(tag_id) REFERENCES tags(id)
-            )",
-            params![],
-        )?;
+        create_schema(&conn)?;
         Ok(conn)
     }
 
