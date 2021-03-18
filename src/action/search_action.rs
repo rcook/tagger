8725eece0bd6465887f7907d764c37ea37791db7
Richard Cook <rcook@rcook.org>
2021-03-18 15:32:22 -0700

    Perform item_tags database query

--- src/action/search_action.rs
+++ src/action/search_action.rs
@@ -1,12 +1,46 @@
+use rusqlite::params;
+use rusqlite::types::Value;
+use std::rc::Rc;
+
 use crate::db;
 use crate::error::Result;
 use crate::project::Project;
 use crate::tag::Tag;
 
 pub fn do_search(project: &Project, tags: &Vec<Tag>) -> Result<()> {
+    #[derive(Debug)]
+    struct Record {
+        id: i32,
+        item_id: i32,
+        tag_id: i32,
+    }
+
     let conn = project.open_db_connection()?;
     let names = tags.into_iter().map(|x| x.as_str()).collect();
     let tags = db::Tag::all_by_names(&conn, &names)?;
-    println!("tags={:?}", tags);
+
+    let tag_ids = tags.iter().map(|x| x.id).collect::<Vec<_>>();
+    let tag_id_values = Rc::new(
+        tag_ids
+            .iter()
+            .map(|&x| Value::from(x))
+            .collect::<Vec<Value>>(),
+    );
+    let mut stmt =
+        conn.prepare("SELECT id, item_id, tag_id FROM item_tags WHERE tag_id IN RARRAY(?1)")?;
+    let records: Vec<_> = stmt
+        .query_map(params![tag_id_values], |row| {
+            Ok(Record {
+                id: row.get(0)?,
+                item_id: row.get(1)?,
+                tag_id: row.get(2)?,
+            })
+        })?
+        .collect::<rusqlite::Result<_>>()?;
+
+    for record in records {
+        println!("record={:?}", record)
+    }
+
     Ok(())
 }
