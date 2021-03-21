af2b5f4d99bef3801f299a666b1280ebd3c4e3c4
Richard Cook <rcook@rcook.org>
2021-03-20 23:26:01 -0700

    Check that all specified items exist and create tag after validation

--- src/action/tag_action.rs
+++ src/action/tag_action.rs
@@ -1,8 +1,9 @@
+use std::collections::HashMap;
 use std::fmt::Debug;
 use std::path::Path;
 
 use crate::db;
-use crate::error::Result;
+use crate::error::{user_error_result, Result};
 use crate::location::Location;
 use crate::project::Project;
 use crate::tag::Tag;
@@ -13,17 +14,35 @@ pub fn do_tag(
     paths: &Vec<impl AsRef<Path> + Debug>,
 ) -> Result<()> {
     let conn = project.open_db_connection()?;
+
+    let locations = paths
+        .into_iter()
+        .map(|x| Location::from_path(&project.dir, x))
+        .collect::<Result<_>>()?;
+    let items = db::Item::all_by_locations(&conn, &locations)?;
+    if items.len() != locations.len() {
+        let h = items
+            .iter()
+            .map(|x| (&x.location, x))
+            .collect::<HashMap<_, _>>();
+        let missing_locations_str = locations
+            .iter()
+            .filter(|&x| !h.contains_key(&x))
+            .map(|x| x.as_str())
+            .collect::<Vec<_>>()
+            .join(", ");
+        return user_error_result(format!(
+            "No items found at locations {}",
+            missing_locations_str
+        ));
+    }
+
     for tag in tags {
         let _ = db::Tag::upsert(&conn, tag)?;
     }
 
     let names = tags.into_iter().map(|x| x.as_str()).collect();
     let tags = db::Tag::all_by_names(&conn, &names)?;
-    let locations = paths
-        .into_iter()
-        .map(|x| Location::from_path(&project.dir, x))
-        .collect::<Result<_>>()?;
-    let items = db::Item::all_by_locations(&conn, &locations)?;
 
     for item in &items {
         for tag in &tags {
